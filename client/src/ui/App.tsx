00ba4133386fa65d2481de78a00492126d490ffd
Alvin Sim <sim.alvin@gmail.com>
2020-07-16 00:33:20 +1000

    Bootstrap client using React with parcel and Typescript

---
+++ client/src/ui/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <title>Spring React HR</title>
+        <h1>Spring React HR</h1>
+      </div>
+    );
+  }
+}
